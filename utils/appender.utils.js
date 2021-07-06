e72a9d71fb9216950e953519efc5f50529ada81b
Jugnu Agrawal <jugnu@appveen.com>
2021-07-06 17:53:10 +0530

    logs

--- utils/appender.utils.js
+++ utils/appender.utils.js
@@ -1,9 +1,16 @@
 const envConfig = require('../config');
 const { client } = require('./queue.utils');
 
+const logger = global.logger;
+
 function configure(config, layouts) {
     return function (loggingEvent) {
-        client.publish(envConfig.consoleLogQueueName, JSON.stringify(loggingEvent));
+        try {
+            client.publish(envConfig.consoleLogQueueName, JSON.stringify(loggingEvent));
+            logger.debug('Logs Published to Queue');
+        } catch (err) {
+            logger.console.error('Error Publishing Logs', err);
+        }
     };
 }
 
